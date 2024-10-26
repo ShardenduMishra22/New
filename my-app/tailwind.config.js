71d3e4bf7d37259b8ad804d8c13561821e6891f9
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-10-27 03:18:10 +0530

    Updated

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,11 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
+  darkMode: 'class', // Enables dark mode with 'class'
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Scans these files for Tailwind classes
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#3C3248', // Custom background color
+        textLight: '#8D78AD',   // Custom light text color
+        textDark: '#FFFFFF',    // Custom dark mode text color
+      },
+    },
   },
   plugins: [],
-}
+};
